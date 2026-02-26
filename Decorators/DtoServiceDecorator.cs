4e31d4a3011309ef03a5fb39665da6769986fccf
lostb1t <coding-mosses0z@icloud.com>
2026-02-26 17:40:28 +0100

    fix: return of the delete and download option

--- Decorators/DtoServiceDecorator.cs
+++ Decorators/DtoServiceDecorator.cs
@@ -7,8 +7,11 @@ using MediaBrowser.Model.Entities;
 
 namespace Gelato.Decorators;
 
-public sealed class DtoServiceDecorator(IDtoService inner) : IDtoService
+public sealed class DtoServiceDecorator(IDtoService inner, Lazy<GelatoManager> manager)
+    : IDtoService
 {
+    private readonly Lazy<GelatoManager> _manager = manager;
+
     public double? GetPrimaryImageAspectRatio(BaseItem item) =>
         inner.GetPrimaryImageAspectRatio(item);
 
@@ -20,7 +23,7 @@ public sealed class DtoServiceDecorator(IDtoService inner) : IDtoService
     )
     {
         var dto = inner.GetBaseItemDto(item, options, user, owner);
-        Patch(dto, false);
+        Patch(dto, item, false, user);
         return dto;
     }
 
@@ -42,7 +45,7 @@ public sealed class DtoServiceDecorator(IDtoService inner) : IDtoService
         var list = inner.GetBaseItemDtos(items, options, user, owner);
         foreach (var itemDto in list)
         {
-            Patch(itemDto, true);
+            Patch(itemDto, item, true, user);
         }
         return list;
     }
@@ -55,22 +58,43 @@ public sealed class DtoServiceDecorator(IDtoService inner) : IDtoService
     )
     {
         var dto = inner.GetItemByNameDto(item, options, taggedItems, user);
-        Patch(dto, false);
+        Patch(dto, item, false, user);
         return dto;
     }
 
-    private void Patch(BaseItemDto dto, bool isList)
+    static bool IsGelato(BaseItemDto dto)
     {
-        // mark if placeholder
-        if (
-            isList
-            || dto.MediaSources?.Length != 1
-            || dto.Path is null
-            || !dto.MediaSources[0].Path.StartsWith("gelato", StringComparison.OrdinalIgnoreCase)
-        )
-            return;
-        dto.LocationType = LocationType.Virtual;
-        dto.Path = null;
-        dto.CanDownload = false;
+        return dto.LocationType == LocationType.Remote
+            && (
+                dto.Type == BaseItemKind.Movie
+                || dto.Type == BaseItemKind.Episode
+                || dto.Type == BaseItemKind.Series
+                || dto.Type == BaseItemKind.Season
+            );
+    }
+
+    private void Patch(BaseItemDto dto, BaseItem? item, bool isList, User? user)
+    {
+        var manager = _manager.Value;
+        if (item is not null && user is not null && IsGelato(dto) && manager.CanDelete(item, user))
+        {
+            dto.CanDelete = true;
+        }
+        if (IsGelato(dto))
+        {
+            dto.CanDownload = true;
+            // mark if placeholder
+            if (
+                isList
+                || dto.MediaSources?.Length != 1
+                || dto.Path is null
+                || !dto.MediaSources[0]
+                    .Path.StartsWith("gelato", StringComparison.OrdinalIgnoreCase)
+            )
+                return;
+            dto.LocationType = LocationType.Virtual;
+            dto.Path = null;
+            dto.CanDownload = false;
+        }
     }
 }
